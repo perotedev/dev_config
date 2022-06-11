15929f101daf83ee511eff08c153382133bf1822
perotedev <rodripema@hotmail.com>
2022-06-10 21:03:32 -0400

    Fix the function that checks if a package is installed

--- config.sh
+++ config.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
 
-PACKAGES=("docker" "docker-compose" "npm")
+AVAILABLE_SCRIPTS=()
 APPS_NOT_FOUND=()
 APPS_INSTALLEDS=()
 
-# verify if an app is installed
-verifyIfAppIsInstalled(){
-    echo -e "Checking the \e[01;33m$1\e[00m package..."
-    PACK_GREP="$(dpkg -s $1 | grep Package: )"
-    PACK_NAME="${PACK_GREP//Package: /}"
-    
-    if [ "$PACK_NAME" != "$1" ]; then
-        APPS_NOT_FOUND+=("$1")
-    else
+checksIfAppIsInstalled(){
+    SEARCH="$(dpkg-query --show -f='${Status} ${Version}\n' $1 2>/dev/null)"
+
+    if [[ "$SEARCH" == *"ok installed"* ]]; then
+        VERSION_APP="${SEARCH//'install ok '/}"
         APPS_INSTALLEDS+=("$1")
+        echo -e "\e[0;32m* $1\e[00m - $VERSION_APP"
+    else
+        APPS_NOT_FOUND+=("$1")
+        echo -e "\e[0;31m* $1\e[00m - not installed"
     fi
+    sleep 1
+}
+
+checksInstalledsApps(){
+    echo -e "\n🧐 Checking installed tools..."
+
+    for element in "${AVAILABLE_SCRIPTS[@]}"; do
+        checksIfAppIsInstalled $element
+    done
+}
+ 
+checksAvailableAppScripts(){
+    SEARCH_DIR=./apps_scripts
+    echo -e "\n🧐 Checking available scripts..."
+
+    for element in "$SEARCH_DIR"/*; do
+        SCRIPT_NAME="${element//'./apps_scripts/'/}"
+        echo -e "\e[0;33m* $SCRIPT_NAME\e[00m"
+
+        SCRIPT_NAME="${SCRIPT_NAME//'.sh'/}"
+        AVAILABLE_SCRIPTS+=("$SCRIPT_NAME")
+        sleep 1
+    done
 }
 
 
+checksAvailableAppScripts
+checksInstalledsApps
 
+# echo -e "\n${AVAILABLE_SCRIPTS[@]}"
