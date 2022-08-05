93b037c2e75ec09328f60366b4dcc74530c4820c
perotedev <rodripema@hotmail.com>
2022-08-05 09:33:55 -0400

    [HOTFIX] - libpangox not found fixed

--- apps_scripts/anydesk.sh
+++ apps_scripts/anydesk.sh
@@ -15,12 +15,11 @@ else
     echo -e "\n\e[01;32m---|\e[00m Installing \e[01;33manydesk\e[00m package..."
     sleep 0.500
     sudo apt update
+    #  this is if anydesk service don1t works: file libpangox not found 
+    wget http://ftp.us.debian.org/debian/pool/main/p/pangox-compat/libpangox-1.0-0_0.0.2-5.1_amd64.deb
+    sudo apt install ./libpangox-1.0-0_0.0.2-5.1_amd64.deb
     curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/anydesk.gpg
     echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
     sudo apt update
     sudo apt-get install anydesk -y
 fi
-
-#  this is if anydesk service don1t works: file libpangox not found 
-#  wget http://ftp.us.debian.org/debian/pool/main/p/pangox-compat/libpangox-1.0-0_0.0.2-5.1_amd64.deb
-#  sudo apt install ./libpangox-1.0-0_0.0.2-5.1_amd64.deb
